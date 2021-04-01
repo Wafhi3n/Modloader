166e4789008d60261b821486813d42bdb6077154
Wafhi3n <wafhien@gmail.com>
2021-04-01 22:53:31 +0200

    1er commit :o

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # UpdateGitModCiv
+
+Ce script installe les mods BBG/BBS/MPH/BSM dans vos Documents au lieu du steam workshop, vous pouvez donc les unsubs.
+Conserver ce script quelque part et lancer le, il va télécharger les mods dans \Documents\My Games\Sid Meier's Civilization VI\Mods.
+Comme les scripts Powershell ne peuvent pas être exécuté par clic, il va créer son raccourci sur votre Bureau pour pouvoir le faire.
+A chaque lancement il compare le tag actuel avec celui du depot du mod et le met à jour si necessaire
+
+N'oublier pas de débloquer le script après l'avoir lu, car sinon il sera bloqué au lancement!
+
+
+
---
+++ majGitCiv.ps1
@@ -0,0 +1,86 @@
+﻿$git = @(
+         "https://github.com/iElden/BetterBalancedGame.git",
+         "https://github.com/d-jackthenarrator/Civ6-BBS.git",
+         "https://github.com/d-jackthenarrator/Civ6-MPH.git",
+         "https://github.com/d-jackthenarrator/Civ6-BSM.git"
+       )
+$documents=[environment]::getfolderpath("mydocuments")
+$desktop=[environment]::getfolderpath("desktop")
+$dirMod=$documents+"\My Games\Sid Meier's Civilization VI\Mods"
+$env:GIT_REDIRECT_STDERR = '2>&1'
+
+function GetName {
+    param(
+        $GitName
+    )
+     $GitName.Split('/')[-1].Split('.')[0]
+}
+function VerifGit {
+    try
+    {
+        git | Out-Null
+    }
+    catch [System.Management.Automation.CommandNotFoundException]
+    {
+        "Veuillez installer git : https://git-scm.com/"
+        exit 1;
+    }
+}
+
+function VerifAndInstallWithGit {
+    param (
+        $Mod
+    )
+    $DirName=GetName $Mod
+    $TotalPath=$dirMod+"\"+$DirName
+    if (!(Test-Path -Path $TotalPath -PathType Container )) {
+        Write-Host $DirName" - non installé dans :"$TotalPath;
+        Write-Host "installation avec git clone..."
+        Set-Location $dirMod
+        git clone $Mod
+    }          
+}
+
+function Update {
+    param (
+        $Mod
+    )
+    $DirName=GetName $Mod
+    $TotalPath=$dirMod+"\"+$DirName
+    Set-Location $TotalPath
+    git fetch --tags
+    $latesttag=$(git describe --tags (git rev-list --tags --max-count=1))
+    $tagActuel=git describe --tags
+    if($latesttag -ne $tagActuel){
+        Write-Host "Maj necessaire de "$DirName " " $latesttag " vers la" $tagActuel
+        git -c advice.detachedHead=false checkout $latesttag
+    }
+    
+}
+
+function createIcon() {
+    $targetPath = "powershell.exe"
+    $Arguments = " -command `"& '"+$com+"'`""
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($($desktop+"\Civ6-BBG.lnk"))
+    $Shortcut.TargetPath = $targetPath
+    $Shortcut.Arguments   = $Arguments
+    $Shortcut.Save()
+}
+
+
+$git | ForEach-Object {
+    VerifAndInstallWithGit $PSItem
+    Update $PSItem
+}
+
+if(!(Test-Path -Path $($desktop+"\Civ6-BBG.lnk")  -PathType Leaf )){
+    $com=$MyInvocation.MyCommand.Path
+    createIcon
+    Write-Host "Icone crée sur le Bureau : Civ6-BBG!"
+}
+
+Start-Process steam://rungameid/289070
+
+
+ 
