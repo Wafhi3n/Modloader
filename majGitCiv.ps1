5ea616f3247f9624990b4844fc83a561020084fb
Wafhi3n <wafhien@gmail.com>
2021-04-02 17:56:08 +0200

    Add watcher

--- majGitCiv.ps1
+++ majGitCiv.ps1
@@ -9,6 +9,15 @@ $desktop=[environment]::getfolderpath("desktop")
 $dirMod=$documents+"\My Games\Sid Meier's Civilization VI\Mods"
 $env:GIT_REDIRECT_STDERR = '2>&1'
 $shortCutName="Civ6-BBG"
+$com=$MyInvocation.MyCommand.Path
+$voice = New-Object -ComObject Sapi.spvoice
+$voice.rate = 0
+function Elevation {
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -File $com";
+        exit;
+    }
+}
 function GetName {
     param(
         $GitName
@@ -26,7 +35,6 @@ function VerifGit {
         exit 1;
     }
 }
-
 function VerifAndInstallWithGit {
     param (
         $Mod
@@ -40,10 +48,10 @@ function VerifAndInstallWithGit {
         git clone $Mod
     }          
 }
-
 function Update {
     param (
-        $Mod
+        $Mod,
+        $GameLauched
     )
     $DirName=GetName $Mod
     $TotalPath=$dirMod+"\"+$DirName
@@ -52,36 +60,106 @@ function Update {
     $latesttag=$(git describe --tags (git rev-list --tags --max-count=1))
     $tagActuel=git describe --tags
     if($latesttag -ne $tagActuel){
-        Write-Host "Maj necessaire de "$DirName " " $latesttag " vers la" $tagActuel
-        git -c advice.detachedHead=false checkout $latesttag
+        if(!$GameLauched){
+            Write-Host "Maj necessaire de "$DirName " " $latesttag " depuis la" $tagActuel
+            git -c advice.detachedHead=false checkout $latesttag
+        }else{
+            $voice.speak($("Maj necessaire de "+$DirName+" "+$latesttag+" depuis la "+$tagActuel+", veuillez redemarrer Civilisation son script."))
+            Write-Host "Maj necessaire de "$DirName " " $latesttag " depuis la " $tagActuel ", veuillez redemarrer le jeu et ce script."
+        }
+    }else{
+            Write-Host $DirName" est à jour."
     }
-    
 }
-
 function createIcon() {
     $targetPath = "powershell.exe"
     $Arguments = "-ExecutionPolicy Bypass -File $com"
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($($desktop+"\"+$shortCutName+".lnk"))
     $Shortcut.TargetPath = $targetPath
-    $Shortcut.Arguments   = $Arguments
+    $Shortcut.Arguments  = $Arguments
     $Shortcut.Save()
 }
-
-VerifGit
-
-$git | ForEach-Object {
-    VerifAndInstallWithGit $PSItem
-    Update $PSItem
+function updateAllMod(){
+    param(
+        $GameLauched
+    )
+    $git | ForEach-Object {
+        Update $PSItem $GameLauched
+    }
+}
+function verifInstallAllMod(){
+    $git | ForEach-Object {
+        VerifAndInstallWithGit $PSItem 
+    }
 }
 
-if(!(Test-Path -Path $($desktop+"\"+$shortCutName+".lnk")  -PathType Leaf )){
-    $com=$MyInvocation.MyCommand.Path
-    createIcon
-    Write-Host "Icone crée sur le Bureau : Civ6-BBG!"
+function main(){
+    $date=Get-Date
+    $nextCheck=$date.AddMinutes(30);
+    #$nextCheck=$date.AddSeconds(15);
+    
+    VerifGit
+    verifInstallAllMod
+    updateAllMod 0
+    
+    if(!(Test-Path -Path $($desktop+"\"+$shortCutName+".lnk")  -PathType Leaf )){
+        createIcon
+        Write-Host "Icone crée sur le Bureau : Civ6-BBG!"
+    }
+    
+    Start-Process steam://rungameid/289070
+    Start-Sleep -s 30
+
+    While ($true){
+        $LaunchPadProcess = Get-Process "LaunchPad" -ErrorAction SilentlyContinue
+        $Civ6Process = Get-Process "CivilizationVI*" -ErrorAction SilentlyContinue
+        $date=Get-Date
+         
+       
+    
+        if ( $Civ6Process -Or $LaunchPadProcess) {
+            if ( $($date - $nextCheck) -gt 0){
+                Write-Host "Recherche de mise à jour"
+                updateAllMod 1 
+                $nextCheck=$date.AddMinutes(30);
+            }
+        }else {
+            Write-Host "jeu eteint, au revoir" 
+            exit;
+        }
+    
+    
+        # Do things lots
+        
+        # Add a pause so the loop doesn't run super fast and use lots of CPU        
+        Start-Sleep -s 5
+    }
 }
 
-Start-Process steam://rungameid/289070
+main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
 
 
  
