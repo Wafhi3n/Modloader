d9a33a83d0d6a2eb4b55886e61015bbb524b7300
Wafhien <wafhien@gmail.com>
2022-08-30 18:27:56 +0200

    Rebase 1.4.1

--- majGitCiv.ps1
+++ majGitCiv.ps1
@@ -1,29 +1,66 @@
+param(
+    [Parameter()]
+    [String]$isShortcut
+)
+
 $git = @(
-         @("iElden","BetterBalancedGame"),
-         @("57fan","Civ6-BBS-2"),
-         @("iElden","MultiplayerHelper"),
-         @("iElden","BetterSpectatorMod")
-       )    
-$apiurl="https://api.github.com/repos"
-$repoUrl="https://github.com/"
+         "https://github.com/iElden/BetterBalancedGame.git",
+         "https://github.com/57fan/Civ6-BBS-2.git",
+         "https://github.com/iElden/MultiplayerHelper.git",
+         "https://github.com/iElden/BetterSpectatorMod.git"
+       )
 $documents=[environment]::getfolderpath("mydocuments")
 $desktop=[environment]::getfolderpath("desktop")
 $dirMod=$documents+"\My Games\Sid Meier's Civilization VI\Mods"
 $env:GIT_REDIRECT_STDERR = '2>&1'
+$shortCutName="Civ6-BBG"
 $com=$MyInvocation.MyCommand.Path
 $voice = New-Object -ComObject Sapi.spvoice
 $voice.rate = 0
-
-try {
-    . ("$PSScriptRoot/settings.ps1")
-    . ("$PSScriptRoot/CommonFct.ps1")
-    . ("$PSScriptRoot/Git.ps1")
-    . ("$PSScriptRoot/old.ps1")
+#Process.StartInfo.UseShellExecute = true;
+#Process.StartInfo.Verb = "runas";
+function Elevation {
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -File $com";
+        exit;
+    }
 }
-catch {
-    Write-Host "Error while loading supporting PowerShell Scripts" 
+function refreshPath {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
+                ";" +
+                [System.Environment]::GetEnvironmentVariable("Path","User")
 }
-
+function GetName {
+    param(
+        $GitName
+    )
+     $GitName.Split('/')[-1].Split('.')[0]
+}
+function VerifGit {
+    try
+    {
+        git | Out-Null
+    }
+    catch [System.Management.Automation.CommandNotFoundException]
+    {
+        "Installation de git:"
+        winget install --id Git.Git -e --source winget
+        refreshPath
+    }
+}
+function VerifAndInstallWithGit {
+    param (
+        $Mod
+    )
+    $DirName=GetName $Mod
+    $TotalPath=$dirMod+"\"+$DirName
+    if (!(Test-Path -Path $TotalPath -PathType Container )) {
+        Write-Host $DirName" - non installé dans :"$TotalPath;
+        Write-Host "installation avec git clone..."
+        Set-Location $dirMod
+        git clone $Mod
+    }          
+}
 function Update {
     param (
         $Mod,
@@ -33,17 +70,12 @@ function Update {
     $TotalPath=$dirMod+"\"+$DirName
     Set-Location $TotalPath
     git fetch --tags
-    $latesttag=$(LatestTag $mod)
+    $latesttag=$(git describe --tags (git rev-list --tags --max-count=1))
     $tagActuel=git describe --tags
     if($latesttag -ne $tagActuel){
         if(!$GameLauched){
             Write-Host "Maj necessaire de "$DirName " " $latesttag " depuis la" $tagActuel
-            if ($latesttag -ne ""){
-                git -c advice.detachedHead=false checkout $('tags/'+$latesttag)
-            }else{
-                git -c advice.detachedHead=false checkout 
-
-            }
+            git -c advice.detachedHead=false checkout $latesttag
         }else{
             $voice.speak($("Maj necessaire de "+$DirName+" "+$latesttag+" depuis la "+$tagActuel+", veuillez redemarrer Civilisation son script."))
             Write-Host "Maj necessaire de "$DirName " " $latesttag " depuis la " $tagActuel ", veuillez redemarrer le jeu et ce script."
@@ -56,7 +88,7 @@ function Update {
 }
 function createIcon() {
     $targetPath = "powershell.exe"
-    $Arguments = "-ExecutionPolicy Bypass -File $com"
+    $Arguments = "-ExecutionPolicy Bypass -File $com -shortcut"
     $Path=$($desktop+"\"+$shortCutName+".lnk")
     $WshShell = New-Object -comObject WScript.Shell
     $Shortcut = $WshShell.CreateShortcut($Path)
@@ -81,7 +113,10 @@ function main(){
     $date=Get-Date
     $nextCheck=$date.AddMinutes(30);
    
-    VerifGit
+    if ($isShortcut -ne "shotcut"){
+        VerifGit
+    }
+    
 
     $git | ForEach-Object {
         VerifAndInstallWithGit $PSItem;
@@ -95,9 +130,15 @@ function main(){
     }
 
     Write-Host "lancement de CIV6 avec steam..."
-    Start-Process steam://rungameid/289070 %command%
-    Start-Sleep -s 30
+    #$PathLaucher = Get-Item -Path "Registry::\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings\S-1-5-21-2093352941-3441766457-1795707191-1001\Device\HarddiskVolume2\SteamLibrary\steamapps\common\Sid Meier's Civilization VI\2KLauncher\LauncherPatcher.exe" | Select-Object -ExpandProperty Property
+    #$PathCIV6 = Get-Item -Path "Registry::\HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\bam\State\UserSettings\S-1-5-21-2093352941-3441766457-1795707191-1001\\Device\HarddiskVolume2\SteamLibrary\steamapps\common\Sid Meier's Civilization VI\Base\Binaries\Win64Steam\CivilizationVI.exe" | Select-Object -ExpandProperty Property
+    #Write-Host $PathLaucher;
+    #exit 0;
+    
+    write-host $isShortcut
 
+    Start-Process "steam://rungameid/289070"
+    Start-Sleep -s 30
     While ($true){
         $LaunchPadProcess = Get-Process "LaunchPad" -ErrorAction SilentlyContinue
         $Civ6Process = Get-Process "CivilizationVI*" -ErrorAction SilentlyContinue
@@ -116,133 +157,4 @@ function main(){
     }
 }
 
-
-
-function labelNomMod(){
-    param(
-        $mod,
-        $x,
-        $y
-    )
-    $label = New-Object  System.Windows.Forms.Label
-    # Initialize the Label and TextBox controls.
-    $label.Location = New-Object System.Drawing.Point($x,$y);
-    $label.Text = $mod[1];
-    $Font = New-Object System.Drawing.Font("Verdana",20,[System.Drawing.FontStyle]::Italic)
-    # Font styles are: Regular, Bold, Italic, Underline, Strikeout
-    $label.Font = $Font
-
-    $label.Size =New-Object System.Drawing.Size(300, $LABEL_Y_SIZE);
-    $label.BackColor = $([System.Drawing.Color]::blue)
-    return $label
-}
-function buttonCheckMod
-(){
-    param(
-        $mod,
-        $x,
-        $y
-    )
-    $Button = New-Object System.Windows.Forms.Button
-    $Button.Location = New-Object System.Drawing.Size($x,$($y-3.5))
-    $Button.Size = New-Object System.Drawing.Size(150,23)
-    $Button.Text = "Getting Status info..."
-    $Button.BackColor = $([System.Drawing.Color]::red)
-
-    $main_form.Controls.Add($Button)
-    return $Button
-
-}
-
-
-function setPanelMod(){
-    param(
-        $main_form
-    )
-
-    $panelMod = New-Object  System.Windows.Forms.Panel
-    $panelMod.Location = New-Object System.Drawing.Point(0,0);
-    $panelMod.Size = New-Object System.Drawing.Size($PANEL_X_SIZE, $PANEL_Y_SIZE);
-
-
-    $panelMod.BackColor = $([System.Drawing.Color]::lightblue)
-
-    #$labelisOK =   VerifModGit $mod
-
-
-    #$textBox1.Location = New-Object System.Drawing.Point(16,32);
-    #$textBox1.Text = "";
-    #$textBox1.Size = New-Object System.Drawing.Size(152, 20);
- 
-    # Add the Panel control to the form.
-
-
-    $main_form.Controls.Add($panelMod);
-    # Add the Label and TextBox controls to the Panel.
-    #$panel1.Controls.Add($textBox1);
-    
-
-
-
-    return $panelMod
-}
-#main
-function addModToPanel(){
-    param(
-        $panelMod,
-        $mod,
-        [int]$position
-    )
-    #Write-Host $position 
-    [int]$y = ($position-1)*$LABEL_Y_SIZE +3*$position;
-    [int]$xButtonCheckMod = 350 + $LABEL_X_SIZE;
-    $labelNomMod = labelNomMod $mod $LABEL_X_SIZE $y
-    $ButtonCheckMod = buttonCheckMod $mod $xButtonCheckMod $y
-    $panelMod.Controls.Add($labelNomMod);
-    $panelMod.Controls.Add($ButtonCheckMod);
-}
-function addLaunchButtonToForm(){
-    param(
-        $main_form
-    )
-    $Button = New-Object System.Windows.Forms.Button;
-
-    #[double]$y = [double]$WINDOW_Y_SIZE - 32
-    #[double]$x = [double]$WINDOW_X_SIZE 
-$x= 200
-$y = 200
-    $Button.Location = New-Object System.Drawing.Size($x, $y)
-    $Button.Size = New-Object System.Drawing.Size(150,23)
-    $Button.Text = "LANCER CIVILIZATION"
-    $Button.BackColor = $([System.Drawing.Color]::red)
-
-    $main_form.Controls.Add($Button)
-    return $Button
-}
-
-
-function mainUI(){
-    Add-Type -assembly System.Windows.Forms
-    Add-Type -assembly System.Drawing
-
-    $main_form = New-Object System.Windows.Forms.Form
-    $main_form.Text ='Launcher Civ-FR'
-    $main_form.Width = $WINDOW_X_SIZE
-    $main_form.Height = $WINDOW_Y_SIZE
-    $main_form.AutoSize = $false
-
-    $panelMod = setPanelMod $main_form;
-    [int]$cpt=0
-    $git | ForEach-Object {
-        $cpt++
-        addModToPanel $panelMod $PSItem $cpt
-    }
-    $launchButton = addLaunchButtonToForm $main_form
-
-
-    $main_form.ShowDialog()
-
-}
-
-#mainUI
-checkInstallFont
+main
